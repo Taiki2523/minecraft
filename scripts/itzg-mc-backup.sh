0b9546d04ea6c3a1e31221699698f469ba0d56c6
Taiki2523 <main2525user@gmail.com>
2025-06-17 20:16:35 +0900

    [change] backup via crontab

---
+++ scripts/itzg-mc-backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run --rm \
+  --name mc-backup \
+  -v /usr/share/minecraft_mnt/minecraft-data:/data:ro \
+  -v /usr/share/minecraft_mnt/mc_backups:/backups \
+  -e WORLD_SAVE_NAME=world_ver_1 \
+  -e ENABLE_RCON=true \
+  -e RCON_HOST=vanilla \
+  -e RCON_PORT=25575 \
+  -e RCON_PASSWORD=supersecurepassword \
+  -e PRUNE_BACKUPS_DAYS=7 \
+  itzg/mc-backup >> /var/log/mc-backup.log 2>&1
